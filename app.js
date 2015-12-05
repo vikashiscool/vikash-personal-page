005de2fbd41e364b9fcc8412bff23d988a056ced
vikashiscool <vikash_parekh@yahoo.com>
2015-12-04 21:22:38 -0500

    Update experiences, create new styles, create app.js for typed.js, install bower components

---
+++ app.js
@@ -0,0 +1,16 @@
+ $(function(){
+        $(".typewriter").typed({
+            strings: ["I am a web developer.", "I am a scientist.", "I am a data analyst.", "I am a writer.", "I am a reader.", "I am an athlete.", "I am a student.", "I am a mentor.", "I am Vikash."],
+            typeSpeed: 0
+        });
+    });
+
+  // I am a web developer.
+  // I am a scientist.
+  // I am a data analyst.
+  // I am a writer.
+  // I am a reader.
+  // I am an athlete.
+  // I am a student.
+  // I am a mentor.
+  // I am Vikash.
